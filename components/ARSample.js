535942e04f8add09768241eee3eaa5e66e7d2f88
aqeel1211 <aqeelejaz234@gmail.com>
2020-12-22 01:37:47 +0500

    added canvas

--- components/ARSample.js
+++ components/ARSample.js
@@ -6,7 +6,7 @@
  * @flow
  */
 
-import React, { Component } from 'react';
+import React, { Component,useState } from 'react';
 
 import {StyleSheet} from 'react-native';
 
@@ -14,47 +14,123 @@ import {
   ViroARScene,
   ViroText,
   ViroConstants,
+  ViroBox,
+  ViroMaterials,
+  Viro3DObject,
+  ViroAmbientLight,
+  ViroSpotLight,
+  ViroARPlaneSelector,
+  ViroNode,
+  ViroPolyline
 } from '@akadrimer/react-viro';
 
-export default class ARSample extends Component {
 
-  constructor() {
-    super();
+export default function ARSample (props) {
 
-    // Set initial state here
-    this.state = {
-      text : "Initializing AR..."
-    };
+  // constructor() {
+  //   super();
 
-    // bind 'this' to functions
-    this._onInitialized = this._onInitialized.bind(this);
-  }
+  //   // Set initial state here
+  //   // this.state = {
+  //   //   // text : "Initializing AR...",
+  //   //   line:[[0,0,0]]
+  //   // };
+  //   this.line = [[0,0,0]]
 
-  render() {
-    return (
-      <ViroARScene onTrackingUpdated={this._onInitialized} >
-        <ViroText text={this.state.text} scale={[.5, .5, .5]} position={[0, 0, -1]} style={styles.helloWorldTextStyle} />
-      </ViroARScene>
-    );
-  }
+  //   // bind 'this' to functions
+  //   this._onInitialized = this._onInitialized.bind(this);
+  //   this._onTouch = this._onTouch.bind(this)
+  //   this._onDrag = this._onDrag.bind(this)
+  // }
 
-  _onInitialized(state, reason) {
-    if (state == ViroConstants.TRACKING_NORMAL) {
-      this.setState({
-        text : "Hello World!"
-      });
-    } else if (state == ViroConstants.TRACKING_NONE) {
-      // Handle loss of tracking
+  // render() {
+    const [line,setLine] = useState([[0,0,0]])  
+    const [text,setText] = useState('Initializing....')
+
+    var styles = StyleSheet.create({
+      helloWorldTextStyle: {
+        fontFamily: 'Arial',
+        fontSize: 30,
+        color: '#ffffff',
+        textAlignVertical: 'center',
+        textAlign: 'center',
+      },
+    });
+    
+    
+    ViroMaterials.createMaterials({
+      grid: {
+        diffuseTexture: require('./res/res/grid_bg.jpg'),
+      },
+      white:{
+        diffuseColor:"white"
+      }
+    });
+
+    function _onDrag(dragToPos, source){
+      console.log(dragToPos[0])
+      console.log(dragToPos[1])
+      console.log(dragToPos[2])
+      console.log('------')
+      
+      let temp = [...line]
+      temp.push([dragToPos[0],dragToPos[1],dragToPos[2]])
+
+      console.log(line)
+
+      setLine(temp)
+      
     }
-  }
-}
 
-var styles = StyleSheet.create({
-  helloWorldTextStyle: {
-    fontFamily: 'Arial',
-    fontSize: 30,
-    color: '#ffffff',
-    textAlignVertical: 'center',
-    textAlign: 'center',
-  },
-});
+    function _onInitialized(state, reason) {
+      if (state == ViroConstants.TRACKING_NORMAL) {
+        setText('Hello World')
+      } else if (state == ViroConstants.TRACKING_NONE) {
+        // Handle loss of tracking
+      }
+    }
+
+    function _onTouch(state, touchPos, source){
+      if(state == 1) {  
+        console.log('touch down') 
+      } else if(state == 2) {
+        console.log('touch down move')
+      } else if(state == 3) { 
+        console.log('Bitch!') 
+      }
+    }
+
+    function _onCameraARHitTest(results) {
+      if(results.hitTestResults.length > 0) {
+        for (var i = 0; i < results.hitTestResults.length; i++) {
+            let result = results.hitTestResults[i];
+            if (result.type == "ExistingPlaneUsingExtent" || result.type == "FeaturePoint") {
+              let temp = [...line]
+              console.log(temp)
+              temp.push(result.transform.position)
+              setLine(temp)
+              
+              //  points: this.state.points.push(result.transform.position),
+               break;
+           } 
+        }
+     }
+  }
+
+  return (
+    <ViroARScene onTrackingUpdated={_onInitialized} >
+      <ViroARPlaneSelector onPlaneSelected={()=>{
+                                              setLine([[0,0,0]])
+                                              console.log('plane selected')}}>
+        <ViroNode position={[0,-1,0]} dragType="FixedToWorld" onTouch = {_onTouch} >
+          <ViroBox position={[0, 0, -.25]} scale={[.1, .1, .1]} />
+        </ViroNode>
+        {/* <ViroPolyline position={[0,0,-.25]} points={line} thickness={0.1}
+        materials={"white"} /> */}
+         {/* <ViroPolyline position={[0,0,0]} points={line} thickness={0.1}
+        materials={"white"} /> */}
+      </ViroARPlaneSelector>
+    </ViroARScene>
+    );
+  
+}
